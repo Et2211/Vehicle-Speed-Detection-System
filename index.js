14153cd38bd4eac2fda3334494795a6ec530050c
Et2211 <up817427@myport.ac.uk>
2020-02-24 19:29:16 +0000

    Added database support

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let PythonShell = require("python-shell");
 let chokidar = require("chokidar");
 let OCR = require("./OCR.js");
 let Tesseract = require("tesseract.js");
+let MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
 let unmatchedPlates = {
   camera1: {},
   camera2: {}
@@ -9,6 +11,9 @@ let unmatchedPlates = {
 let plates = {};
 let dir = process.cwd();
 let id = 0
+const dbName = 'Plates'
+let db
+let distance = 100 //in metres
 
 /* console.log("What distance apart are the cameras in metres?")
 let distance = console.readline() //doesn't work with node!
@@ -33,7 +38,7 @@ PythonShell.PythonShell.run("finalPrototype.py", options, function (err) {
     });
     //plate = result.source
   } catch (e) {
-    //console.log(e);
+    console.log(e);
     console.log("Python message: " + message);
   }
 });
@@ -49,8 +54,9 @@ function storePlate(plate, camera, time) {
       let plateToMove = unmatchedPlates["camera" + cam][plate]  
       plateToMove.time2 = time;
       plateToMove.timeDifference = Date.parse(plateToMove.time2) - Date.parse(plateToMove.time1)
+      plateToMove.speed = calcSpeed(plateToMove.timeDifference)
       plates[id] = plateToMove
-      console.log(unmatchedPlates[plate])
+      addtoDB(plateToMove)
       delete unmatchedPlates["camera" + cam][plate] 
       
       console.log("unMatched plates are: ")
@@ -112,4 +118,48 @@ let readChars = function (img) {
 function getPlateCamera(plate) {
   if (unmatchedPlates.camera1.hasOwnProperty(plate)) return 1
   return 2
+}
+
+function connectToDB(){
+  
+
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err) return console.log(err)
+
+    // Storing a reference to the database so you can use it later
+    db = client.db(dbName)
+    console.log(`Connected MongoDB: ${url}`)
+    console.log(`Database: ${dbName}`)
+  }    
+)}
+
+function calcSpeed(time){
+  return (distance/time) * 2236.94 //converts to mph
+}
+
+function addtoDB(platetoStore) {
+  console.log("plate to add to DB:")
+  console.log(platetoStore)
+ 
+  let plate = platetoStore.plate
+  let time1 = platetoStore.time1
+  let time2 = platetoStore.time2
+  let timeDifference = platetoStore.timeDifference
+  let speed = platetoStore.speed
+  
+  connectToDB()
+  const collection = db.collection('plates');
+  // Insert some documents
+  collection.insert(
+    {
+      plate : plate,
+      time1 : time1,
+      time2 : time2,
+      timeDifference : timeDifference,
+      speed : speed
+    }
+  , function(err, result) {
+      if (err) console.log(err)
+      console.log("Inserted plate into the collection"); 
+  });
 }
