711def1a1112ea61ea7058ae0bdb0aa4ca174463
Et2211 <up817427@myport.ac.uk>
2020-04-18 23:27:44 +0100

    New Hash algorithm

--- OCR.js
+++ OCR.js
@@ -1,6 +1,18 @@
 let Tesseract = require('tesseract.js')
 let x11 = require('@dashevo/x11-hash-js');
 
+Object.defineProperty(String.prototype, 'hashCode', {
+    value: function() {
+      var hash = 0, i, chr;
+      for (i = 0; i < this.length; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+      }
+      return hash;
+    }
+  });
+
 let readChars = async function(img) {
   return new Promise(function(resolve, reject) {
       Tesseract.recognize(img, "eng",
@@ -14,6 +26,7 @@ let readChars = async function(img) {
           }) => {
               text = text.replace(/[^a-zA-Z0-9]/g, "");
               //text = x11.digest(text) //Hash algorithm for the plate
+              text = text.hash()
               console.log(text)
               resolve(text);
           }
