c424083095291a09fa3beb2c432d3cc2bcb45fe3
Et2211 <up817427@myport.ac.uk>
2020-05-05 02:26:13 +0100

    Separate DB interface

---
+++ DBinterface.js
@@ -0,0 +1,36 @@
+let MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
+const dbName = 'Plates'
+let db
+
+function connectToDB() {
+    MongoClient.connect(url, {
+        useNewUrlParser: true
+    }, (err, client) => {
+        if (err) return console.log(err)
+
+        // Storing a reference to the database so you can use it later
+        db = client.db(dbName)
+        console.log(`Connected MongoDB: ${url}`)
+        console.log(`Database: ${dbName}`)
+    })
+}
+
+function addtoDB(platetoStore) {
+
+    const collection = db.collection('plates');
+    collection.insertOne({
+        plate: platetoStore.plate,
+        time1: platetoStore.time1,
+        time2: platetoStore.time2,
+        timeDifference: platetoStore.timeDifference,
+        speed: platetoStore.speed
+    }, function(err, result) {
+        if (err) console.log(err)
+        console.log("Inserted plate into the collection");
+    });
+}
+module.exports = {
+    addtoDB : addtoDB,
+    connectToDB : connectToDB
+  }
